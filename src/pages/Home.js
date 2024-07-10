eaa3d13235192516a6d41d8826dc61fce541bab1
Nasser Mughrabi <nasser@Nassers-MacBook-Air.local>
2024-07-09 19:11:27 -0600

    fixed mobile view for netlify

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,15 @@
 import { VStack } from "@chakra-ui/react";
 import ListingModal from "../components/listings/ListingModal";
 import Listings from "../components/listings/Listings";
-// import EC2 from "../components/common/EC2";
+import EC2 from "../components/common/EC2";
 
 const Home = () => {
   
   return (
     <VStack py={4}>
-      {/* <EC2 /> */}
-      <ListingModal />
-      <Listings />
+      <EC2 />
+      {/* <ListingModal />
+      <Listings /> */}
     </VStack>
   );
 };
